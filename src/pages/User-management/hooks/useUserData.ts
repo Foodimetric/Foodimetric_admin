28953280836c3136023ce0c42ed904f29835b710
Zion <omojolazion@gmail.com>
2025-08-21 18:05:40 +0100

    Foodimetric Dashboard v1

--- src/pages/User-management/hooks/useUserData.ts
+++ src/pages/User-management/hooks/useUserData.ts
@@ -1,30 +1,30 @@
-import { useEffect, useState } from "react";
-import { User } from "../../../pages/User-management/types/user";
+// import { useEffect, useState } from "react";
+// import { User } from "../../../pages/User-management/types/user";
 
-export const useUserData = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+// export const useUserData = () => {
+//   const [users, setUsers] = useState<User[]>([]);
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    // Dummy data
-    setTimeout(() => {
-      setUsers([
-        {
-          id: "1",
-          firstName: "User",
-          lastName: "One",
-          email: "user1@example.com",
-          usage: 120,
-          category: 1,
-          googleId: "google-uid-1",
-          credits: 500,
-          lastUsageDate: "2025-07-20",
-          verified: true,
-        },
-      ]);
-      setIsLoading(false);
-    }, 1000);
-  }, []);
+//   useEffect(() => {
+//     // Dummy data
+//     setTimeout(() => {
+//       setUsers([
+//         // {
+//         //   id: "1",
+//         //   firstName: "User",
+//         //   lastName: "One",
+//         //   email: "user1@example.com",
+//         //   usage: 120,
+//         //   category: 1,
+//         //   googleId: "google-uid-1",
+//         //   credits: 500,
+//         //   lastUsageDate: "2025-07-20",
+//         //   verified: true,
+//         // },
+//       ]);
+//       setIsLoading(false);
+//     }, 1000);
+//   }, []);
 
-  return { users, isLoading };
-};
+//   return { users, isLoading };
+// };
